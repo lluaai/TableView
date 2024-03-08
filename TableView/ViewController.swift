7e993eaa97082b5ca098e15bb94a89c214abb14b
Arailym <asermagambet@mail.ru>
2024-03-08 14:06:44 +0500

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Арайлым Сермагамбет on 08.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
